c76aafad64dbe59a90599f849acad3db10dafcad
akushni <akushni@uwo.ca>
2024-05-21 15:54:13 -0400

    implemented degree p ar process

--- cleanedUpCode/allFunctions_SimAndProc.py
+++ cleanedUpCode/allFunctions_SimAndProc.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import queue
 
 from discreteKStest import *
 from findMaxDev import *
@@ -8,10 +9,15 @@ from findMaxDev import *
 neutral = 0
 stepFunc = 1
 autoReg = 2
+autoRegP = 3
 
-# implemented transfomrations
+# implemented transformations
 fishersAngular = 0
 variance = 1
+
+# KS tests
+discrete = 0
+continuous = 1
 
 # step function selection structure
 def s_stepFunction(t, swapPoint, c):
@@ -20,9 +26,31 @@ def s_stepFunction(t, swapPoint, c):
     else:
         return - c
     
-# auto regressive selection structure
+# auto regressive (degree 1) selection structure
 def s_autoReg(c,s):
     return c * s + np.random.normal(0,0.01,len(s))
+
+# degree p auto regressive selection structure
+def s_autoRegP(sVals, p, t):
+    
+    coeffs = np.zeros(p)
+    for i in range(p): # draw weights from geometric distribution with p = 0.? and centered at 0
+        coeffs[i] = sp.stats.geom.pmf(i, 0.8, -1) 
+    
+    recentSVals = np.zeros((p, sVals.shape[1]))
+    
+    if t < p: # if less than p generations have passed
+        for i in range(1, t+1):
+            recentSVals[i-1,:] = sVals[t-i,:]
+    else:
+        for i in range(1, p+1):
+            recentSVals[i-1] = sVals[t-i,:]
+        
+    newSVals = np.zeros(sVals.shape[1])
+    for i in range(sVals.shape[1]):
+        newSVals[i] = np.sum(coeffs * recentSVals[:, i]) + np.random.normal(0,0.01)
+        
+    return newSVals
 
 # simulate trajectories
 def simulation(N, t, numTrajs, p0, sType, c = None):
@@ -65,6 +93,22 @@ def simulation(N, t, numTrajs, p0, sType, c = None):
 
             trajectories[i,:] = p + s * p * (1 - p) # save new frequencies (adjusted for selection)
 
+
+    elif sType == autoRegP:
+        
+        sVals = np.zeros((t + 1, numTrajs)) # initialize array to store s values
+        sVals[0,:] = np.random.normal(0, 0.01, numTrajs)
+        trajectories[0, :] = p0 + sVals[0,:] * p0 * (1 - p0) # save first set of frequencies (adjusted for selection)
+        
+        for i in range(1, t+1):
+            n = np.random.binomial(N, trajectories[i-1]) # draw from binomial
+            p = n/N
+        
+            s = s_autoRegP(sVals, 10, i)
+           
+            trajectories[i,:] = p + s * p * (1 - p) # save new frequencies (adjusted for selection)
+            sVals[i] = s # save s values
+        
     return trajectories
 
 # apply Fisher's Angular transform and permute
--- cleanedUpCode/procForSim.py
+++ cleanedUpCode/procForSim.py
@@ -1,20 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sp
+import os
 
 from allFunctions_SimAndProc import *
 
 ###################################################################
 
-N = 10000 # population size
-t = 200 # number of generations
-numTrajectories = 500 # number of trajectories
-p0 = np.random.uniform(0.3, 0.7, numTrajectories) # initial frequencies
+N = 1000 # population size
+t = 60 # number of generations
+numTrajectories = 3*(10**6) # number of trajectories
+p0 = np.random.uniform(0.4, 0.9, numTrajectories) # initial frequencies
 
-sType = autoReg
+sType = autoRegP # neutral, autoReg, autoRegP or stepFunc
 stepStrength = 0.01 # strength of selection if using step function
-autoRegConst = 0.3 # proportion of previous value maintained if using auto regressive process
+autoRegConst = 0.6 # proportion of previous value maintained if using auto regressive process
 
-transformationType = fishersAngular
+transformationType = fishersAngular # fishersAngular or variance
+
+ksTestType = continuous # discrete or continuous
+
+coarseGrain = True # True or False
+
+filterOutFixed = True # True or False
 
 ###################################################################
 
@@ -25,8 +33,26 @@ elif sType == stepFunc:
     trajectories = simulation(N, t, numTrajectories, p0, sType, stepStrength)
 elif sType == autoReg:
     trajectories = simulation(N, t, numTrajectories, p0, sType, autoRegConst)
+elif sType == autoRegP:
+    trajectories = simulation(N, t, numTrajectories, p0, sType)
+    
+# coarse graining
+if coarseGrain:
+    tempArray = np.zeros((int(t/10)+1, numTrajectories))
+    tempArray[0,:] = trajectories[0,:]
+    for i in range(1,int(t/10)+1):
+        tempArray[i,:] = trajectories[i*10,:]
+    trajectories = tempArray
+    t = int(t/10) 
+    
+# filter out trajectories that fix
+if filterOutFixed:
+    tempArray = trajectories[:,trajectories[-1,:] != 1]
+    tempArray = tempArray[:, tempArray[-1,:] != 0]
+    trajectories = tempArray
+    numTrajectories = trajectories.shape[1]
 
-# transform
+# transform and procedure
 if transformationType == fishersAngular:
     FisherTrajs, newTrajs, reverseTransform = procWithFT(trajectories, numTrajectories, t)
 elif transformationType == variance:
@@ -37,5 +63,9 @@ origDevs = findMaxDevs(trajectories, trajectories[0,:], numTrajectories)
 transformedDevs = findMaxDevs(reverseTransform, reverseTransform[0,:], numTrajectories)
 
 # KS test
-pVal = discreteKStest(origDevs, transformedDevs)
+if ksTestType == discrete:
+    pVal = discreteKStest(origDevs, transformedDevs)
+elif ksTestType == continuous:
+    pVal = sp.stats.kstest(origDevs, transformedDevs).pvalue
 
+os.system('say finished')
